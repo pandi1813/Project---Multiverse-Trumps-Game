ae0434f8c7ecab2a0ce74a06955f8ce2380f20fc
pandi1813 <andrea.peter1813@gmail.com>
2023-02-02 20:11:24 +0000

    js update: added variables for DOM buttons and sections and used event listener to hide start screen when the player clickes on play btn

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-.then(data => console.log(data.images))
+let rulesBtn = document.querySelector("#rule-btn")
+let startBtn = document.querySelector("#play-btn")
+let startScreen = document.querySelector("#start-section")
+let gameScreen = document.querySelector("game-section")
+
+startBtn.addEventListener("click", function () {
+    startScreen.setAttribute("class", "hide");
+
+   
+})
