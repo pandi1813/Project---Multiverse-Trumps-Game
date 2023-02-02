1de04b772a738f4ace052eb5195cd48cb0d8decb
EdgarsCer <Edgarscernijs1993@gmail.com>
2023-02-02 21:08:38 +0000

    added variables to store kaywords , funstion to see results , function to generate gif

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-.then(data => console.log(data.images))
+let gameResultLose = ["youlose", "looser", "sad", "tryme"]
+let gameResultWin = ["winning", "victory", "cheers", "youwin", "cry"]
+let gameResultTie = ["look", "really", "oh"]
+let gameResult = ""
+
+
+let score = "tie"
+winOrLose()
+
+
+function winOrLose() {
+    if (score == "win") {
+        gameResult = gameResultWin[0]
+    }
+    else if (score == "lose") {
+        gameResult = gameResultLose[0]
+    }
+    else {
+        gameResult = gameResultTie[2]
+    }
+
+    getGif(gameResult)
+}
+
+function getGif(gifWord) {
+
+    fetch(`https://api.gfycat.com/v1/gfycats/search?search_text=${gifWord}`)
+        .then(response => response.json())
+        .then(data => {
+            let selectedGif = data.gfycats[2].max2mbGif;
+            let gifEl = document.querySelector("#gif");
+            gifEl.src = selectedGif;
+        })
+}
+
+
+
+
