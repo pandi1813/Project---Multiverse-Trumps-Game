76521a08a0680dcc5b9694af6edd5b6ddd48c49b
pandi1813 <andrea.peter1813@gmail.com>
2023-02-02 20:37:08 +0000

    created start screen base layout and display game screen after clicking play btn

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let rulesBtn = document.querySelector("#rule-btn")
 let startBtn = document.querySelector("#play-btn")
 let startScreen = document.querySelector("#start-section")
-let gameScreen = document.querySelector("game-section")
+let gameScreen = document.querySelector("#game-section")
 
 startBtn.addEventListener("click", function () {
-    startScreen.setAttribute("class", "hide");
+    startScreen.classList.add("hide");
 
-   
+    gameScreen.classList.remove("hide");
+    
 })
