e9e023f5e83d474f1fe4f5827766638c33c84e5a
cl.lee <cl.lee.ofz@gmail.com>
2023-02-02 19:32:48 +0000

    added pseudo codes and fetch codes to superheroes API

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-.then(data => console.log(data.images))
+// .then(data => console.log(data.images))
+
+// get random hero by id (no. 1 - 731)
+    // Math.random 
+// get hero name
+// hero image
+// hero powers
+
+fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/1.json")
+.then(response => response.json())
+.then(heroData => console.log(heroData))
