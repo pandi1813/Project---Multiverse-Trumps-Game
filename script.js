ed3cdaa38a49de72895c79122621c9dcc5891f0f
pandi1813 <andrea.peter1813@gmail.com>
2023-02-02 20:48:58 +0000

    created rules page and used event listeners to display it and return to start screen when buttons clicked

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-let rulesBtn = document.querySelector("#rule-btn")
-let startBtn = document.querySelector("#play-btn")
-let startScreen = document.querySelector("#start-section")
-let gameScreen = document.querySelector("#game-section")
+let rulesBtn = document.querySelector("#rule-btn");
+let startBtn = document.querySelector("#play-btn");
+let returnBtn = document.querySelector("#return-btn");
 
+
+let startScreen = document.querySelector("#start-section");
+let gameScreen = document.querySelector("#game-section");
+let rulesScreen = document.querySelector("#rules-section");
+
+
+
+
+//************************* EVENT LISTENERS ***************************
 startBtn.addEventListener("click", function () {
     startScreen.classList.add("hide");
-
     gameScreen.classList.remove("hide");
-    
 })
+
+
+
+rulesBtn.addEventListener("click", function () {
+    startScreen.classList.add("hide");
+    rulesScreen.classList.remove("hide");
+})
+
+
+returnBtn.addEventListener("click", function () {
+    rulesScreen.classList.add("hide");
+    startScreen.classList.remove("hide");
+})
+
+
+
