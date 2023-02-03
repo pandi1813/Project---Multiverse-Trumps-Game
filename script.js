5f984651565bdaa9866619cf74cdb7440b809afb
pandi1813 <andrea.peter1813@gmail.com>
2023-02-03 17:56:44 +0000

    added notes to js and css files

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// *********************** VARIABLES ***********************************
+
+// variables for buttons
 let rulesBtn = document.querySelector("#rule-btn");
 let startBtn = document.querySelector("#play-btn");
 let returnBtn = document.querySelector("#return-btn");
 
-
+// variables for screen containers
 let startScreen = document.querySelector("#start-section");
 let gameScreen = document.querySelector("#game-section");
 let rulesScreen = document.querySelector("#rules-section");
@@ -11,19 +14,21 @@ let rulesScreen = document.querySelector("#rules-section");
 
 
 //************************* EVENT LISTENERS ***************************
+
+// start screen --> game screen
 startBtn.addEventListener("click", function () {
     startScreen.classList.add("hide");
     gameScreen.classList.remove("hide");
 })
 
 
-
+// start screen --> ruses screen
 rulesBtn.addEventListener("click", function () {
     startScreen.classList.add("hide");
     rulesScreen.classList.remove("hide");
 })
 
-
+// rules screen --> start screen
 returnBtn.addEventListener("click", function () {
     rulesScreen.classList.add("hide");
     startScreen.classList.remove("hide");
