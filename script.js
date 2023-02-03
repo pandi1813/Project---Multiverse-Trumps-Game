3b3ce8ae998f4a6bf05a7898c1d16bc1e30df07f
cl.lee <cl.lee.ofz@gmail.com>
2023-02-03 16:24:49 +0000

    consolidated get random superhero's data into one function

--- script.js
+++ script.js
@@ -2,27 +2,44 @@
 fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
     .then(response => response.json())
     .then(heroData => {
-        
-        // pull a random superhero's data
-        function getRandomHero(heroData) {
-        let randomHeroIndex = Math.floor(Math.random() * heroData.length);
-        return heroData[randomHeroIndex];
+
+        // get a random superhero's data (for display on the card)
+        function getHeroData(heroData) {
+            
+            // select a random superhero from the data
+            let randomHeroIndex = Math.floor(Math.random() * heroData.length);
+            let randomHero = heroData[randomHeroIndex];
+
+            // get superhero name
+            let heroName = randomHero.name;
+
+            // get superhero image (available image sizes: xs , sm, md, lg)
+            let imageURL = randomHero.images.sm;
+
+            // get superhero power stats
+            let intelligenceStat = randomHero.powerstats.intelligence;
+            let strengthStat = randomHero.powerstats.strength;
+            let speedStat = randomHero.powerstats.speed;
+            let durabilityStat = randomHero.powerstats.durability;
+            let powerStat = randomHero.powerstats.power;
+            let combatStat = randomHero.powerstats.combat;
+
+            return {
+                heroName,
+                imageURL,
+                intelligenceStat,
+                strengthStat,
+                speedStat,
+                durabilityStat,
+                powerStat,
+                combatStat,
+            };
         }
-        let randomHero = getRandomHero(heroData);
+        
+        let selectedHero = getHeroData(heroData);
+        console.log(selectedHero);
 
-        // get superhero name
-        let heroName = randomHero.name;
 
-        // get superhero image (available image sizes: xs , sm, md, lg)
-        let imageURL = randomHero.images.sm;
-
-        // get superhero power stats
-        let intelligenceStat = randomHero.powerstats.intelligence;
-        let strengthStat = randomHero.powerstats.strength;
-        let speedStat = randomHero.powerstats.speed;
-        let durabilityStat = randomHero.powerstats.durability;
-        let powerStat = randomHero.powerstats.power;
-        let combatStat = randomHero.powerstats.combat;
 
         // put data into card
 
