e4f9bc6ce40da14a197826a1c4a0007519001f11
EdgarsCer <Edgarscernijs1993@gmail.com>
2023-02-03 19:14:28 +0000

    now gif is generated randomly from arrays  also more keyworkds added

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 let gameResultLose = ["youlose", "looser", "sad", "tryme"]
-let gameResultWin = ["winning", "victory", "cheers", "youwin", "cry"]
-let gameResultTie = [,"muhaha","ha", "haha","goodluck", "really"]
+let gameResultWin = ["winning", "victory", "cheers"]
+let gameResultTie = [, "muhaha", "ha", "haha", "goodluck", "really"]
 let gameResult = ""
 
-
-let score = "tie"
-winOrLose()
+let text = "";
+let score = "win";
+winOrLose();
 
 
 function winOrLose() {
-    if (score == "win") {
-        const randomWin = Math.floor(Math.random() * gameResultWin.length)
-        gameResult = gameResultWin[randomWin]
-    }
-    else if (score == "lose") {
-        const randomLose = Math.floor(Math.random() * gameResultLose.length)
-        gameResult = gameResultLose[randomWin]
-    }
-    else {
-        const randomTie = Math.floor(Math.random() * gameResultTie.length)
-        gameResult = gameResultTie[randomTie]
+    if (score === "win") {
+        const randomWin = Math.floor(Math.random() * gameResultWin.length);
+        gameResult = gameResultWin[randomWin];
+        text = "Congratulations You Won !";
+    } else if (score === "lose") {
+        const randomLose = Math.floor(Math.random() * gameResultLose.length);
+        gameResult = gameResultLose[randomLose];
+        text = "Sorry, You Lost !";
+    } else {
+        const randomTie = Math.floor(Math.random() * gameResultTie.length);
+        gameResult = gameResultTie[randomTie];
+        text = "It's a Tie !";
     }
 
-    getGif(gameResult)
+    let textEl = document.createElement("h1");
+    textEl.textContent = text;
+    document.querySelector("#resultText").appendChild(textEl);
+
+    getGif(gameResult);
 }
 
 function getGif(gifWord) {
@@ -35,8 +40,6 @@ function getGif(gifWord) {
             gifEl.src = selectedGif;
         })
 }
-
-// try this   --->  https://api.gfycat.com/v1/reactions/populated?tagName=
 
 
 
